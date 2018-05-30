9ec917b2903c99d853105a59656d6b28dcb0778b
xpc <xinpeicheng@yeah.net>
2018-05-30 11:36:41 +0800

    修复bug

--- routes/route.js
+++ routes/route.js
@@ -2,5 +2,6 @@ const router = require('koa-router')(),
   bookAPI = require('./api/getbook.api.js');
 router
   .all('/api/getBookList', bookAPI.getBookList)
-  .all('/api/getBook', bookAPI.getBook);
+  .all('/api/getBook', bookAPI.getBook)
+  .all('/api/getChaptertext', bookAPI.getChaptertext);
 module.exports = router;
